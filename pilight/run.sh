2de2bc9eba5decb75357349b8f1f22a2bcd2a0d4
Philipp Luettecke <philipp.luettecke@googlemail.com>
2020-10-07 13:59:03 +0200

    Update run.sh

--- pilight/run.sh
+++ pilight/run.sh
@@ -4,6 +4,5 @@ echo Here we would like to adapt the pilight config with values from the configu
 
 echo Starting pilight daemon
 
+echo 11 > /sys/class/gpio/export
 /usr/local/sbin/pilight-daemon -F
-#service pilight restart
-

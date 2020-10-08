fe0b88b924a077efd2e80ddd5220e0a8ea117d99
Philipp Luettecke <philipp.luettecke@googlemail.com>
2020-10-08 09:36:48 +0200

    Update run.sh

--- pilight/run.sh
+++ pilight/run.sh
@@ -6,8 +6,10 @@ CONFIG_PATH=/data/options.json
 GPIO_PLATFORM=$(jq -r '.gpio_platform // empty' "$CONFIG_PATH")
 GPIO_PLATFORM=${GPIO_PLATFORM:-none} # default to none
 
+echo GPIO Platform found: $GPIO_PLATFORM
+
 # Update pilight config
-sed -i 's/\("gpio-platform"\): \?".*"\(.*\)/\1: "'"$GPIO_PLATFORM"'"\2/' /etc/pilight/config.json
+#sed -i 's/\("gpio-platform"\): \?".*"\(.*\)/\1: "'"$GPIO_PLATFORM"'"\2/' /etc/pilight/config.json
 
 echo Starting pilight daemon
 #echo 11 > /sys/class/gpio/export

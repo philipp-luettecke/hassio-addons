dc667d61ae79c1a92a58c2cae8d52afefc53a0c6
philipp-luettecke <philipp.luettecke@gmail.com>
2020-11-02 17:36:53 +0100

    Update Run.sh of Jenkins Addon

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/with-contenv bashio
 
 echo Starting Jenkins daemon
-#/etc/init.d/jenkins start
-/bin/su -l jenkins --shell=/bin/bash \
--c '/usr/bin/daemon --name=jenkins \
---inherit --env=JENKINS_HOME=/var/lib/jenkins \
---output=/var/log/jenkins/jenkins.log \
---pidfile=/var/run/jenkins/jenkins.pid -- \
-/usr/bin/java  -Dhudson.diyChunking=false \
--Djenkins.install.runSetupWizard=false -jar \
-/usr/share/jenkins/jenkins.war \
---webroot=/var/cache/jenkins/war \
---httpPort=8080'
+
+java -jar /opt/jenkins.war

fe7c44b833505760642ba04883d18c110f7cee39
philipp-luettecke <philipp.luettecke@gmail.com>
2020-11-03 18:09:47 +0100

    Update pilight

--- pilight/run.sh
+++ pilight/run.sh
@@ -3,11 +3,8 @@
 echo Here we would like to adapt the pilight config with values from the configuration of the addon
 
 GPIO_PLATFORM="$(bashio::config 'gpio_platform')"
-GPIO_PLATFORM=${GPIO_PLATFORM:-none} # default to none
 SENDER="$(bashio::config 'sender')"
-SENDER=${SENDER:-'-1'} # default to -1
 RECEIVER="$(bashio::config 'receiver')"
-RECEIVER=${RECEIVER:-'-1'} # default to -1
 
 
 echo GPIO Platform found: $GPIO_PLATFORM

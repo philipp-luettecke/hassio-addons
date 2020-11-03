aff9431aa7084cfdc20261a5008d5fc7ea2dd87c
philipp-luettecke <philipp.luettecke@gmail.com>
2020-11-03 18:23:33 +0100

    update run.sh

--- pilight/run.sh
+++ pilight/run.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/with-contenv bashio
 
-echo Here we would like to adapt the pilight config with values from the configuration of the addon
+bashio::log Here we would like to adapt the pilight config with values from the configuration of the addon
 
 GPIO_PLATFORM="$(bashio::config 'gpio_platform')"
-SENDER="$(bashio::config 'sender')"
-RECEIVER="$(bashio::config 'receiver')"
+
+if bashio::config.has_value "gpio_platform"; then GPIO_PLATFORM=$(bashio::config 'gpio_platform');
+else
+bashio::log.error "No Platform specified - using none instead"
+GPIO_PLATFORM="none"
+fi
+
+if bashio::config.has_value "sender"; then SENDER=$(bashio::config 'sender'); else SENDER=-1; fi
+if bashio::config.has_value "receiver"; then RECEIVER=$(bashio::config 'receiver'); else RECEIVER=-1; fi
 
 
-echo GPIO Platform found: $GPIO_PLATFORM
-echo SENDER Pin is: $SENDER
-echo RECEIVER Pin is: $RECEIVER
+bashio::log "GPIO Platform found: $GPIO_PLATFORM"
+bashio::log "SENDER Pin is: $SENDER"
+bashio::log "RECEIVER Pin is: $RECEIVER"
 
 # Update pilight config
 sed -i 's/\("gpio-platform"\): \?".*"\(.*\)/\1: "'"$GPIO_PLATFORM"'"\2/' /etc/pilight/config.json
 sed -i 's/\("sender"\): \?".*"\(.*\)/\1: "'"$SENDER"'"\2/' /etc/pilight/config.json
 sed -i 's/\("receiver"\): \?".*"\(.*\)/\1: "'"$RECEIVER"'"\2/' /etc/pilight/config.json
 
-echo Starting pilight daemon
+bashio::log.green "Starting pilight daemon"
+
 /usr/local/sbin/pilight-daemon -F
